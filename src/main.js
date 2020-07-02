8e61bd43e87f3d709c334b2b034551bc05c7012f
LucaKlingler <lucaklingler@outlook.de>
2020-07-02 09:49:53 +0200

    set ut (die basics)

--- src/main.js
+++ src/main.js
@@ -1,8 +1,24 @@
 import Vue from 'vue'
+import store from './store'
 import App from './App.vue'
-
+import VueSocketIO from 'vue-socket.io'
+import SocketIO from "socket.io-client"
+ 
 Vue.config.productionTip = false
 
+Vue.use(new VueSocketIO({
+    debug: true,
+    connection: 'http://metinseylan.com:1992',
+    vuex: {
+        store,
+        actionPrefix: 'SOCKET_',
+        mutationPrefix: 'SOCKET_'
+    },
+    options: { path: "/my-app/" } //Optional options
+}))
+ 
 new Vue({
-  render: h => h(App),
+    router,
+    store,
+    render: h => h(App)
 }).$mount('#app')
